0c2603505896b636f204b804d87898a97e6d5332
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2023-01-19 23:14:33 -0800

    Add comments, display SVG

--- gallery.js
+++ gallery.js
@@ -1,21 +1,26 @@
 class Slides {
   constructor() {
-    this.slides = [];
-    this.currentSlideIndex = 0;
+    this.slides = []; // array to hold the slide elements
+    this.currentSlideIndex = 0; // keep track of the current slide
     this.baseUrl = 'https://floaties.s3.amazonaws.com/img/'
-    this.isPlaying = false
+    this.isPlaying = false // flag to indicate if the slideshow is playing
   }
 
   // create an individual slide from image data
   createSlide(data) {
     const img = document.createElement("img");
     img.setAttribute("src", `${this.baseUrl}${data.name}`);
+    if(data.name.includes("svg")) {
+      img.setAttribute('width', '1200px')
+    } 
+    img.setAttribute("sizes", "(min-width: 800px) 50vw, 100vw")
     const div = document.createElement("div");
     div.classList.add("slides");
     div.appendChild(img);
     return div;
   }
 
+  // render the slides to the DOM
   renderSlides(imgData) {
     const slideshowContainer = document.querySelector(".slideshow-container");
     slideshowContainer.replaceChildren();
@@ -26,16 +31,19 @@ class Slides {
     });
   }
 
+  // move to the next or previous slide
   navigateSlides(direction) {
     this.showSlides(direction);
   }
-
+  
+  // set the current slide index
   setSlideIndex(newIndex = 0) {
     this.currentSlideIndex = newIndex
   }
 
+  // show the current slide and hide all other slides
   showSlides(direction = 0) {
-    this.slides = this.getSlidesElement()
+    this.slides = this.getSlidesElement() // load our slides into the global object
     let slides = this.slides;
     let newIndex = this.currentSlideIndex + direction;
     this.albumLength = slides.length
@@ -51,35 +59,11 @@ class Slides {
       slides[this.currentSlideIndex].classList.remove('fade')
     }
     slides[this.currentSlideIndex].style.display = "block";
-    // this.showFraction(this.currentSlideIndex, slides.length)
   }
 
+  // return the slide element from the DOM
   getSlidesElement() {
     return document.getElementsByClassName("slides")
-  }
-
-  shuffle(arr) {
-    let i = arr.length
-    let j = 0
-    let temp = {}
-
-    while (i--) {
-        j = Math.floor(Math.random() * (i+1));
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    return arr;
-  }
-
-  shuffleIndexes(arr) {
-    let indexes = arr.map( (e,i) => i)
-    return this.shuffle(indexes)
-  }
-
-  showFraction(num, denom){
-    const slideFractionDiv = document.getElementsByClassName("slide-fraction")[0]
-    slideFractionDiv.innerHTML = `${num + 1} / ${denom}`
   }
 }
 
@@ -94,20 +78,24 @@ class Album {
     this.images = data.images;
     this.template = data.content_template
     this.albumLength = 0 // we set this in extractImageData
-    this.timeOutId = 0
+    this.albumIndex = 0
   }
 
-  loadAlbum() {
+  // render the album and update the display of the album's data
+  loadAlbum(albumIndex) {
     this.updateAlbumDataDisplay()
-    this.renderImages()
+    this.getImageDataForRendering()
+    this.albumIndex = albumIndex
   }
 
+  // extract image references from the content template
   extractImageReferences(contentTemplate) {
     const re = /{.*}/g
     const editedTemplate = contentTemplate.replaceAll('}{', '}\n{')
     return editedTemplate.match(re)
   }
   
+  // remove the scale value from the image reference
   removeScale(imageReference) {
     if (imageReference.includes('scale')) {
       const ix = imageReference.search(' scale')
@@ -117,6 +105,7 @@ class Album {
     }
   }
   
+  // extract image data from the template
   extractImageData(contentTemplate, images) {
     const imageReferences = this.extractImageReferences(contentTemplate)
     const cleanedReferences = imageReferences.map(this.removeScale)
@@ -126,6 +115,7 @@ class Album {
     return cleanedReferences.map(ref => imageLookup[ref])
   }
 
+  // clean the formatting in the attribution field
   cleanAttribution(attribution) {
     const re = /by([^\s:])/
     const matches = attribution.match(re)
@@ -136,6 +126,7 @@ class Album {
     return attribution
   }
 
+  // format album data into an HTML string, then parse into DOM element
   formatAlbumData(title, titleAttr, worksAttr, tags, link) {
     let albumData = `<div>`;
     albumData += `<p class="album-title">${title}</p>`;
@@ -151,6 +142,7 @@ class Album {
     return htmlToElement(albumData);
   }
 
+  // get album fields from an album
   getAlbumData(album) {
     this.title = album.title;
     this.tags = album.tags;
@@ -162,29 +154,24 @@ class Album {
     this.template = album.content_template         
   }
 
+  // update the album data on screen
   updateAlbumDataDisplay() {
     const albumDataContainer = document.getElementsByClassName("album-data")[0];
     albumDataContainer.replaceChildren();
     const albumData = this.formatAlbumData(this.title, this.titleAttr, this.worksAttr, this.tags, this.link);
     albumDataContainer.appendChild(albumData);
-    // this.showFraction(this.albumIndex, this.totalAlbums)
   }
 
-  renderImages() {
+  // get image data for rendering
+  getImageDataForRendering() {
     const imageData = this.extractImageData(this.template, this.images)
     this.albumLength = imageData.length
     return imageData
-  }
-
-  showFraction(num, denom){
-    const albumFractionDiv = document.getElementsByClassName("album-fraction")[0]
-    albumFractionDiv.innerHTML = `${num + 1} / ${denom}<br /><br />`
   }
 }
 
 class Slideshow {
   constructor(floaties) {
-    this.albumIndex = 0;
     this.slideIndex = 0;
     this.albumCollection = floaties; // array of albums
     this.randomAlbums = this.shuffleIndexes(this.albumCollection); // [7, 4, 1, etc.] values correspond to place in albumCollection
@@ -193,8 +180,10 @@ class Slideshow {
     this.playMode = ""
     this.slides = new Slides();
     this.album = new Album(this.albumCollection[0])
+    this.timeOutId = 0
   }
 
+  // fisher-yates shuffle
   shuffle(arr) {
     let i = arr.length
     let j = 0
@@ -209,33 +198,56 @@ class Slideshow {
     return arr;
   }
 
+  // use fisher-yates to shuffle indexes of a given array
   shuffleIndexes(arr) {
     let indexes = arr.map( (e,i) => i)
     return this.shuffle(indexes)
   }
 
+  // move to next or previous album
   navigateAlbum(direction = 0) {
-    let newIndex = this.albumIndex + direction;
-    this.albumIndex = newIndex < 0 ? this.albumCollection.length - 1 : newIndex % this.albumCollection.length;
-    this.loadAlbum(this.albumIndex)
+    let newIndex = this.album.albumIndex + direction;
+    this.album.albumIndex = newIndex < 0 ? this.albumCollection.length - 1 : newIndex % this.albumCollection.length;
+    this.loadAlbum(this.album.albumIndex)
     this.displaySlides()
+    this.updateAlbumCounter()
   }
 
+  // gets album data and loads it into the state
   loadAlbum(albumIndex = 0) {
     this.album.getAlbumData(this.albumCollection[albumIndex])
-    this.album.loadAlbum()
+    this.album.loadAlbum(albumIndex)
+    this.updateAlbumCounter()
   }
 
+  // renders the slides from the album, shows current slide and counter
   displaySlides(slideIndex = 0) {
-    this.slides.renderSlides(this.album.renderImages())
+    this.slides.renderSlides(this.album.getImageDataForRendering())
     this.slides.setSlideIndex(slideIndex)
     this.slides.showSlides()
+    this.updateSlidesCounter(this.slides.currentSlideIndex, this.slides.slides.length)
   }
 
+  // navigate next/previous slide
   navigateSlides(direction = 0) {
     this.slides.navigateSlides(direction);
+    this.updateSlidesCounter(this.slides.currentSlideIndex, this.slides.slides.length)
+
   }
 
+  // update album counter
+  updateAlbumCounter(){
+    const albumCounterDiv = document.getElementsByClassName("album-counter")[0]
+    albumCounterDiv.innerHTML = `${this.album.albumIndex + 1} / ${this.albumCollection.length}<br /><br />`
+  }
+
+  // update slide counter
+  updateSlidesCounter(){
+    const slideCounterDiv = document.getElementsByClassName("slide-counter")[0]
+    slideCounterDiv.innerHTML = `${this.slides.currentSlideIndex + 1} / ${this.slides.slides.length}`
+  }
+
+  // sets state variables before playing, resets them when paused
   playSlideshow(playbackOption) {
     const isPlaying = !this.isPlaying;
     this.isPlaying = isPlaying;
@@ -244,23 +256,25 @@ class Slideshow {
 
     if(this.isPlaying) {
         this.playSlides(playbackOption);
-        // console.log(this.slides.getSlidesElement()[this.slides.currentSlideIndex].classList)
     } else {
-        this.playMode = ""
-        // console.log(this.slides.getSlidesElement()[this.slides.currentSlideIndex].classList)
-        this.slides.getSlidesElement()[this.slides.currentSlideIndex].classList.remove('fade')
-        if(playbackOption != 'random'){
-          this.slides.showSlides(-1)
-        }
-        clearTimeout(this.timeOutId)
+      clearTimeout(this.timeOutId)
+      this.playMode = ""
+
+      if (this.slides.currentSlideIndex == this.slides.slides.length) {
+        this.slides.currentSlideIndex -= 1
+      }
+      this.slides.getSlidesElement()[this.slides.currentSlideIndex].classList.remove('fade')
+      if(playbackOption != 'random'){
+        this.slides.showSlides(-1)
+      }
     }
     this.toggleNavigation(playbackOption);
   }
 
-
+  // main slideshow loop
   playSlides() {
     if(this.isPlaying) {
-      this.timeOutId = setTimeout(() => this.playSlides(this.playMode), 6000)
+      this.timeOutId = setTimeout(() => this.playSlides(this.playMode), 9000)
     
       if(this.playMode == 'set') {
         this.playImageSet()
@@ -272,42 +286,50 @@ class Slideshow {
     }
   }
 
+  // play through the entire collection
   playAllSets() {
       if (this.slides.currentSlideIndex == this.album.albumLength) {
-        this.albumIndex += 1
-        this.albumIndex = this.albumIndex == this.albumCollection.length ? 0 : this.albumIndex;
+        this.album.albumIndex += 1
+        this.album.albumIndex = this.album.albumIndex == this.albumCollection.length ? 0 : this.album.albumIndex;
 
-        this.album.getAlbumData(this.albumCollection[this.albumIndex])
-        this.album.loadAlbum(this.albumIndex)
+        this.album.getAlbumData(this.albumCollection[this.album.albumIndex])
+        this.album.loadAlbum(this.album.albumIndex)
+        this.updateAlbumCounter()
         this.displaySlides()
       }
       this.playImageSet()
   }
-
+  
+  // slideshow of random images
   playRandomImages() {
     this.getRandomAlbum()
-    let randomImageIx = this.shuffleIndexes(this.albumCollection[this.albumIndex]['images'])[0]
+    let randomImageIx = this.shuffleIndexes(this.albumCollection[this.album.albumIndex]['images'])[0]
     this.displaySlides(randomImageIx)
     this.randomAlbumsIx += 1
   }
 
+  // loop through a set
   playImageSet() {
       this.displaySlides(this.slides.currentSlideIndex)
       this.slides.setSlideIndex(this.slides.currentSlideIndex + 1)
   }
 
+  // gets a random album's data and loads it
   getRandomAlbum() {
-    this.albumIndex = this.randomAlbums[this.randomAlbumsIx]
-    this.album.getAlbumData(this.albumCollection[this.albumIndex])
-    this.album.loadAlbum(this.albumIndex)
+    this.album.albumIndex = this.randomAlbums[this.randomAlbumsIx]
+    this.album.getAlbumData(this.albumCollection[this.album.albumIndex])
+    this.album.loadAlbum(this.album.albumIndex)
+    this.updateAlbumCounter()
     this.randomAlbumsIx += 1
   }
 
+  // shows a random album 
   randomAlbum() {
     this.getRandomAlbum()
     this.displaySlides()
   }
 
+  // turn navigation on or off when the slideshow is playing
   toggleNavigation(currentPlayBackOption = "") {
     const slideNavigation = document.querySelector(".slide-navigation-container")
     const albumNavigation = document.querySelector(".album-navigation")
@@ -327,16 +349,6 @@ class Slideshow {
       }
     });
   }
-
-  showFraction(option, num, denom){
-    if(option == 'slides') {
-      const slideFractionDiv = document.getElementsByClassName("slide-fraction")[0]
-      slideFractionDiv.innerHTML = `${num + 1} / ${denom}`
-    } else {
-      const albumFractionDiv = document.getElementsByClassName("album-fraction")[0]
-      albumFractionDiv.innerHTML = `${this.albumIndex + 1} / ${denom}<br /><br />`
-    }
-  }
 }
 
 
@@ -346,15 +358,15 @@ function htmlToElement(html) {
   return template.content.children[0];
 }
 
-async function fetchAllData(albumIndex, slideIndex) {
+async function fetchAllData() {
   const response = await fetch("https://floaties.s3.us-west-1.amazonaws.com/floaties.json");
-  const json = await response.json();
-  return json
+  const data = await response.json();
+  return data.reverse()
 }
 
 async function loadFunctionality() {
-  const floaties = await fetchAllData(0,0)
-  const slideshow = new Slideshow(floaties)
+  const data = await fetchAllData()
+  const slideshow = new Slideshow(data)
 
   const prevSlide = document.getElementById('prev-slide');
   prevSlide.addEventListener('click', () => slideshow.navigateSlides(-1), false );
